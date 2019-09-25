441fa5138a1d81be6cce2c5a1689789bc49371e9
Paulo Abreu <paullooabreu@gmail.com>
2019-09-25 17:44:05 -0300

    Faltam 4 aulas

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,20 +1,38 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform'
+import * as Yup from 'yup';
 import logo from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 
+import { signUpRequest } from '../../store/modules/auth/actions'
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().min(6, 'A senha deve ter no mínimo 6 caracteres').required('A senha é obrigatória'),
+});
+
 export default function SignUp() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <form>
-        <input placeholder="Nome Completo" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha secreta" />
 
         <button type="submit">Criar Conta</button>
         <Link to="/">Já possuo conta</Link>
-      </form>
+      </Form>
     </>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { persistStore } from 'redux-persist';
+import createSagaMiddleware from 'redux-saga';
+import createStore from './createStore';
+import persistReducer from './persistReducer';
+
+import rootReducer from './modules/rootReducer';
+import rooSaga from './modules/rootSaga';
+
+const sagaMonitor = process.env.NODE_ENV === 'development'
+  ? console.tron.createSagaMonitor()
+  : null;
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+const middlewares = [sagaMiddleware];
+
+const store = createStore(persistReducer(rootReducer), middlewares);
+const persistor = persistStore(store);
+
+sagaMiddleware.run(rooSaga);
+
+export { store, persistor };
